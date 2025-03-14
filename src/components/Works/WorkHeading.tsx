130151b227fba1efdcf05f7867d9236377e46d51
Abhinav Saxena <abhinavsaxena@Abhinavs-MacBook-Air.local>
2025-03-14 18:45:10 +0530

    fixing jenkinsfile

--- src/components/Works/WorkHeading.tsx
+++ src/components/Works/WorkHeading.tsx
@@ -1,5 +1,5 @@
 export default function WorkHeading(){
     return(
-        <h1 className="text-center text-5xl text-white mt-5 mb-10">Recent  <span style={{color: '#06d6a0'}}>Works</span></h1>
+        <h1 className="text-center text-5xl text-white mt-5 mb-10">Recent  <span style={{color: '#06d6a0'}}>Worksssss</span></h1>
     )
 }
